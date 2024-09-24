c257e729d8966e1f3f69e6a282ed70c15155887a
Sonia Matyjaszczyk <aphexsonia@gmail.com>
2024-09-24 20:06:17 +0200

    Add initproject.py

--- initproject.py
+++ initproject.py
@@ -4,3 +4,6 @@ shopping_dict = {
     "piekarnia" : ["chleb", "paczek", "bulki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+for shop in shopping_dict:
+    items = [item.capitalize() for item in shopping_dict[shop]]
+    print(f"Idę do {shop.capitalize()}, kupuję tam: {', '.join(items)}.")
