3613cb776b1d911650f9985a233922535fe3c018
Sonia Matyjaszczyk <aphexsonia@gmail.com>
2024-09-24 19:54:15 +0200

    Add initproject.py

---
+++ initproject.py
@@ -0,0 +1,6 @@
+print("Lista zakupów")
+shopping_list = ["chleb", "paczek", "bulki", "marchew", "seler", "rukola"]
+shopping_dict = {
+    "piekarnia" : ["chleb", "paczek", "bulki"],
+    "warzywniak" : ["marchew", "seler", "rukola"]
+}
